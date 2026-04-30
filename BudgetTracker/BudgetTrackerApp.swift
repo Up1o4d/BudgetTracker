03d0fb93fc3f6f32a64d5af816713f481021e248
Up1o4d <up1o4d@proton.me>
2026-04-30 08:38:16 +0200

    Initial commit

---
+++ BudgetTracker/BudgetTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetTrackerApp.swift
+//  BudgetTracker
+//
+//  Created by user on 30. 4. 26.
+//
+
+import SwiftUI
+
+@main
+struct BudgetTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetTrackerTests/BudgetTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  BudgetTrackerTests.swift
+//  BudgetTrackerTests
+//
+//  Created by user on 30. 4. 26.
+//
+
+import Testing
+
+struct BudgetTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
