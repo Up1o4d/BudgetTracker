606f7c99b513b0d4a5f8cd9d473c01fd60bf2a07
Up1o4d <up1o4d@proton.me>
2026-08-23 07:28:35 +0200

    Display suggested vendors on AddView snapshot tests

--- BudgetTrackerTests/SnapshotTests/AddViewSnapshotTests.swift
+++ BudgetTrackerTests/SnapshotTests/AddViewSnapshotTests.swift
@@ -72,6 +72,16 @@ struct AddViewSnapshotTests {
 
     private func makeDummyTransactions() -> [BudgetTracker.Transaction] {
         return [
+            Transaction(id: "1", amount: 54.32, vendor: "Whole Foods", categoryId: Category.groceries.id, date: Date(timeIntervalSince1970: 1_699_000_000)),
+            Transaction(id: "2", amount: 1_200, vendor: "Landlord LLC", categoryId: Category.rent.id, date: Date(timeIntervalSince1970: 1_698_900_000)),
+            Transaction(id: "3", amount: 12.75, vendor: "Uber", categoryId: Category.transport.id, date: Date(timeIntervalSince1970: 1_698_800_000)),
+            Transaction(id: "4", amount: 28.40, vendor: "Chipotle", categoryId: Category.dining.id, date: Date(timeIntervalSince1970: 1_698_700_000)),
+            Transaction(id: "5", amount: 89.10, vendor: "Pacific Gas & Electric", categoryId: Category.utilities.id, date: Date(timeIntervalSince1970: 1_698_600_000)),
+            Transaction(id: "6", amount: 15.99, vendor: "Netflix", categoryId: Category.other.id, date: Date(timeIntervalSince1970: 1_698_500_000)),
+            Transaction(id: "7", amount: 42.18, vendor: "Trader Joe's", categoryId: Category.groceries.id, date: Date(timeIntervalSince1970: 1_698_400_000)),
+            Transaction(id: "8", amount: 61.00, vendor: "Whole Foods", categoryId: Category.groceries.id, date: Date(timeIntervalSince1970: 1_698_300_000)),
+            Transaction(id: "9", amount: 9.50, vendor: "Uber", categoryId: Category.transport.id, date: Date(timeIntervalSince1970: 1_698_200_000)),
+            Transaction(id: "10", amount: 33.60, vendor: "Chipotle", categoryId: Category.dining.id, date: Date(timeIntervalSince1970: 1_698_100_000)),
         ]
     }
 }
